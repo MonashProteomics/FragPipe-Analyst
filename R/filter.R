d835fa87dcfcdba93fd7eb4b7ddc5a2104eacbf8
Yi Hsiao <hsiaoyi0504@gmail.com>
2022-11-15 20:49:46 -0500

    add missing filter.R

---
+++ R/filter.R
@@ -0,0 +1,20 @@
+global_filter <- function(se, percentage=50){
+  percentage <- percentage / 100
+  ridx <- rowSums(is.na(assay(se))) / ncol(assay(se)) <= percentage
+  se <- se[ridx,]
+  return(se)
+}
+
+filter_by_condition <- function(se, min_percentage=50) {
+  min_percentage <- min_percentage / 100
+  conditions <- unique(colData(se)$condition)
+  row_ids <- rep(0, nrow(assay(se)))
+  for (c in conditions){
+    se_c <- se[,colData(se)$condition == c]
+    ridx <- rowSums(!is.na(assay(se_c))) / ncol(assay(se_c)) >= min_percentage
+    row_ids <- row_ids + ridx
+  }
+  print(row_ids)
+  se <- se[row_ids > 0,]
+  return(se)
+}
