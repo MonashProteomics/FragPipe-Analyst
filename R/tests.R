eee9f899f9edd75cc7711b69ad25a611392613f0
Yi Hsiao <hsiaoyi0504@gmail.com>
2023-12-18 20:20:59 -0500

    remove unused function

--- R/tests.R
+++ R/tests.R
@@ -17,44 +17,6 @@ exp_design_test<-function(exp_design){
   
 }
 
-### Test if column names are proper in maxquant ProteinGroups file
-maxquant_input_test<-function(maxquant_input){
-  col_names<-colnames(maxquant_input)
-  ## 
-  if(!"Gene.names" %in% col_names){
-    stop(safeError("The column 'Gene names' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (any(grepl("LFQ", col_names))==FALSE){
-    stop(safeError("Columns starting with 'LFQ' are not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (!"Protein.IDs" %in% col_names){
-    stop(safeError("The column 'Protein IDs' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (!"Reverse" %in% col_names){
-    stop(safeError("The column 'Reverse' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (!"Potential.contaminant" %in% col_names){
-    stop(safeError("The column 'Potential contaminant' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (!"Only.identified.by.site" %in% col_names){
-    stop(safeError("The column 'Only identified by site' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (!"Razor...unique.peptides" %in% col_names){
-    stop(safeError("The column 'Razor + unique peptides' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-  else if (!"Protein.names" %in% col_names){
-    stop(safeError("The column 'Protein names' is not found in the MaxQuant proteinGroups File"))
-  }
-  
-}
-
 # mimic the original maxquant part (exp_design_test)
 # temp2 <- read.table("../test-LFQ-Analyst/lfq_manifest.fp-manifest", sep="\t", stringsAsFactors = F)
 fragpipe_manifest_test<-function(fragpipe_manifest){
